6b3a673857a1f65867edb29684bfe6ae17635f05
Maximiliano Cóppola <maxicopp@outlook.com>
2022-12-02 01:04:25 -0300

    Testing Asynchronous code with callbacks

---
+++ async-code/async/async-example.test.js
@@ -0,0 +1,18 @@
+import { expect, it } from "vitest";
+
+import { generateToken } from "./async-example";
+
+it("should generate a token value", (done) => {
+  const testUserEmail = "test@test.com";
+
+  generateToken(testUserEmail, (err, token) => {
+    done();
+    try {
+      expect(token).toBeDefined();
+      // expect(token).toBe(2);
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
