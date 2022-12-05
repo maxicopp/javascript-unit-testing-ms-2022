164e0e199942ccae48103af498573e2ec455c8cc
Maximiliano Cóppola <maxicopp@outlook.com>
2022-12-05 01:52:40 -0300

    Mocking in selected places

--- mocking-diving-deeper/util/http.test.js
+++ mocking-diving-deeper/util/http.test.js
@@ -1,5 +1,6 @@
 import { expect, it, vi } from "vitest";
 
+import { HttpError } from "./errors";
 import { sendDataRequest } from "./http";
 
 const testResponseData = { testKey: "testData" };
@@ -41,4 +42,24 @@ it("should convert the provided data to JSON before sending the request", async 
   }
 
   expect(errorMessage).not.toBe("Not a string.");
+});
+
+it("should throw an HttpError in case of non-ok responses", () => {
+  testFetch.mockImplementationOnce((url, options) => {
+    return new Promise((resolve, reject) => {
+      const testResponse = {
+        ok: false,
+        json() {
+          return new Promise((resolve, reject) => {
+            resolve(testResponseData);
+          });
+        },
+      };
+      resolve(testResponse);
+    });
+  });
+
+  const testData = { key: "test" };
+
+  return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError);
 });
